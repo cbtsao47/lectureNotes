0c891d612b4dc117e821b6a182f9f1a84d993a0c
wisdomtooth <cbtsao47@gmail.com>
2018-12-02 00:12:49 -0600

    add control flow

---
+++ ControlFlow.js
@@ -0,0 +1,29 @@
+// control flow
+
+// if... else statements
+
+// if(condition){
+//     do this
+// }else{
+//     do this
+// }
+
+//switch statements
+
+// switch (condition){
+//     case condition:
+//         do this
+//         break;
+//     case condition:
+//         do this
+//         break;
+//     case condition:
+//         do this
+//         break;
+//     default:
+//         do this
+// }
+
+//for loops
+//for in loops
+//for of loops
