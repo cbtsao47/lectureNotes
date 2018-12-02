19dcd9c13a1886807cd06e003ac90461ae4a94f4
wisdomtooth <cbtsao47@gmail.com>
2018-12-01 23:48:17 -0600

    add operators

---
+++ operators.js
@@ -0,0 +1,53 @@
+// operators + values  => expression
+// expression + expression => algorithm
+
+//arithmetic operator
+// +
+// -
+// *
+// /
+// %
+
+// Increment
+let x = 0;
+x++; // increased after
+++x; // increased before
+
+//decrement
+x--; // decreased after
+--x; // decreased before
+
+//comparison operator
+
+// >
+// >=
+// <
+// <=
+//euqality operator
+// ==
+// ===
+
+// ternary operator
+// condition ? expression: expression
+
+// logical operators
+// or operator , if one is true, it will be true
+true || true;
+true || false;
+false || true;
+false || false;
+// and operator, if one is false, it will be false
+true && true;
+true && false;
+false && true;
+false && false;
+
+// Truthy/Falsy values
+// falsy
+// undefinednull
+// 0
+// false
+// ''
+// NaN
+
+// Anything that's not falsy is truthy
